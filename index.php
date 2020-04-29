d1bd0e2622dae9d508e2849f68c8b91afee9b278
Clayton Radke <claytonradke@gmail.com>
2020-04-29 16:40:48 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movie Quotes</title>
+</head>
+<body>
+    <h1>TechCareers Favourite Films</h1>
+</body>
+</html>
